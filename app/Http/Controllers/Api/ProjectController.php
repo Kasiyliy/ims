79b99e3e3e27f0ea2424c6414e5ea94101c208fc
air <asilkhan-al@mail.ru>
2019-04-14 15:55:46 +0600

    Investment done

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -22,20 +22,23 @@ class ProjectController extends Controller
             return response()->json("Error");
         } else {
 
-            $sql = "select p.id                                                        as project_id,
-       p.overall_price                                             as project_sum,
-       case when sum(i.price) is null then 0 else sum(i.price) end as sobrannie_dengi,
-       (p.overall_price - case when sum(i.price) is null then 0 else sum(i.price) end)                            as ostalos,
-       p.year_profit                                               as godovoy_dohod_za_odin_procent,
-       (p.overall_price - case when sum(i.price) is null then 0 else sum(i.price) end) * 100 / p.overall_price    as ostalos_procentov,
-       $request->money * 100 / p.overall_price                                 as vawi_dengi_procentah_projecta,
-       $request->money * 100 / p.overall_price * p.year_profit                 as skolko_poluchite,
-       YEAR(P.end_date) - year(now())                              as god
+            $sql = "select p.id                                                                                                    as project_id,
+       p.title                                                                                                 as title,
+       p.overall_price                                                                                         as project_sum,
+       case when sum(i.price) is null then 0 else sum(i.price) end                                             as sobrannie_dengi,
+       (p.overall_price -
+        case when sum(i.price) is null then 0 else sum(i.price) end)                                           as ostalos,
+       p.year_profit                                                                                           as godovoy_dohod_za_odin_procent,
+       (p.overall_price - case when sum(i.price) is null then 0 else sum(i.price) end) * 100 /
+       p.overall_price                                                                                         as ostalos_procentov,
+       $request->money * 100 / p.overall_price                                                                             as vawi_dengi_procentah_projecta,
+       $request->money * 100 / p.overall_price * p.year_profit                                                             as skolko_poluchite,
+       YEAR(P.end_date) - year(now())                                                                          as god
 
 from projects p
        left join investments i on p.id = i.project_id
 where p.sphere_id = $request->sphere_id
-group by p.id, p.overall_price, p.year_profit, p.end_date
+group by p.id, p.title, p.overall_price, p.year_profit, p.end_date
 having ostalos > 0
    and god > 0;";
             $data = DB::select($sql);
