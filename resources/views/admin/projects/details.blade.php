911ba3a990f622b0cc256488679234d52fa2f9d8
air <asilkhan-al@mail.ru>
2019-04-15 14:54:13 +0600

    Project details fixed

--- resources/views/admin/projects/details.blade.php
+++ resources/views/admin/projects/details.blade.php
@@ -10,8 +10,7 @@
                     </div>
                     <div class="panel-body">
                         <h3>Предпрениматель: {{$project->user->name}}</h3>
-
-                        @if($project->overallSum < $project->sum())
+                        @if($project->overall_price >= $project->sum())
                             @if(\Illuminate\Support\Facades\Auth::user()->isInvestor() )
                                 <div class="text-right">
                                     <button type="button" class="btn btn-danger  mr-1" data-toggle="modal"
